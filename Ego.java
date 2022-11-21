c802aca0ce6aa63226c84799caacd054eeec3d5e
Andrea Gentili <93248865+andgent@users.noreply.github.com>
2022-11-21 15:26:57 +0100

    Ego e Funzione risolti

---
+++ Ego.java
@@ -0,0 +1,9 @@
+public class Ego {
+    public static void main(String[] args) {
+
+        String mionome = "Andrea";
+        System.out.println("+ - - - - - +");
+        System.out.println("|   " + mionome + "  |");
+        System.out.println("+ - - - - - +");
+    }
+}
---
+++ Funzione.java
@@ -0,0 +1,19 @@
+public class Funzione {
+public static void main(String[] args) {
+
+        int a = 5;
+        int b = 8;
+    
+        if (a > b) {
+            System.out.println(a);
+        }
+        if (b > a) {
+            System.out.println(b);
+        } else {
+            System.out.println("I valori sono pari");
+        }
+
+        }
+    }
+
+
