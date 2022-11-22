a756a81cc5960767ee7437a7e30f36c88838ab7a
Andrea Gentili <93248865+andgent@users.noreply.github.com>
2022-11-22 09:56:15 +0100

    Pari e Dispari

---
+++ PariDispari.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class PariDispari {
+    
+}
+    public static void main(String[] args) {
+      pari();
+    } 
+
+    public static boolean pari(){
+        boolean pari = true;
+        int valore;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci valore");
+        valore = scanner.nextInt();
+
+        if(valore %2 == 0) {
+            pari = true;
+            System.out.println("Il valore è pari");
+
+        } else { 
+            System.out.println("Il valore è dispari");
+        }
+       scanner.close(); 
+        return pari;
+    }  
+    }
